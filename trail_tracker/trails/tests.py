a8f24592c7e5f51f3752581323f5f4e946d17eba
OllieBuilds <scarborough.oliver@gmail.com>
2017-06-08 21:45:47 -0400

    Create initial test

--- trail_tracker/trails/tests.py
+++ trail_tracker/trails/tests.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import unittest
 from django.test import TestCase
+from trail_tracker.trails.models import Trail
 
-# Create your tests here.
+class TestModel(unittest.TestCase):
+
+    def test_orm_working(self):
+        # Assumes database is empty
+        trail = Trail.objects.all()
+        self.assertEqual(len(trail), 0)
+
+        # Create a sample Trail object
+        trail = Trail(name="Test", city="Boston", state="MA")
+        self.assertEqual(trail.name, "Test")
