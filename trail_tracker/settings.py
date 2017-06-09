68348d3148c12e4805dcddc3bc9bd36a197fdfec
OllieBuilds <scarborough.oliver@gmail.com>
2017-06-08 20:15:50 -0400

    More env stuffs

--- trail_tracker/settings.py
+++ trail_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(gzuxarsa22_5z5@*!4id4^!l39si$8t$_)_0vyfgp5ddggo04'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
